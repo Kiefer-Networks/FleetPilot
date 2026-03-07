52649f3e251afeb43075d301178bbf33ab4bed4d
Malte Kiefer <malte.kiefer@mailbox.org>
2026-03-07 14:07:11 +0100

    chore: configure project dependencies, build settings, and linting
    Add all runtime and dev dependencies (Riverpod, Dio, Freezed, go_router,
    flutter_secure_storage, local_auth, flutter_map, file_picker, etc.).
    Configure Android/iOS build settings, analysis options, and gitignore.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "de.kiefernetwork.fleetpilot"
+    namespace = "de.kiefer_networks.fleetpilot"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "de.kiefernetwork.fleetpilot"
+        applicationId = "de.kiefer_networks.fleetpilot"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/de/kiefernetwork/fleetpilot/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package de.kiefernetwork.fleetpilot
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
